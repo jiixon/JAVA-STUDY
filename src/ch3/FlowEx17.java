a5cca17e0d4311fed4cb24d93ada41ccf269a275
jiixon <jr0107380@gmail.com>
2023-09-17 01:42:44 +0900

    *반복문

---
+++ src/ch3/FlowEx17.java
@@ -0,0 +1,20 @@
+package ch3;
+
+import java.util.Scanner;
+
+public class FlowEx17 {
+    public static void main(String[] args) {
+        int num=0;
+        System.out.print("*을 출력할 라인 수를 입력하세요.>");
+        Scanner scanner = new Scanner(System.in);
+        String tmp = scanner.nextLine(); //입력받은 내용을 tmp에 저장
+        num= Integer.parseInt(tmp); //입력받은 문자열 tmp를 숫자로 변환
+
+        for (int i=0; i<num; i++){
+            for (int j=0; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
