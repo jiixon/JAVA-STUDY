61ba9ede2a1bb925444e5f77a1472938cb353239
jiixon <jr0107380@gmail.com>
2023-09-17 00:33:16 +0900

    swith문

--- src/ch3/OperatorEx25.java
+++ src/ch3/OperatorEx25.java
@@ -8,7 +8,7 @@ public class OperatorEx25 {
         char ch = ' ';
         System.out.printf("문자를 하나 입력하세요.>");
         String input = scanner.nextLine();
-        ch = input.charAt(0);
+        ch = input.charAt(0); //입력받은 문자열 input에서 0번째 문자를 ch에 저장
 
         if('0'<=ch && ch <= '9') {
             System.out.printf("입력하신 문자는 숫자입니다.%n");
