725f63b56c758ef1e79d222a3e724aaeac7efe92
jiixon <jr0107380@gmail.com>
2023-09-20 19:04:39 +0900

    flowEx32

---
+++ src/ch3/FlowEx32.java
@@ -0,0 +1,28 @@
+package ch3;
+
+import java.util.Scanner;
+
+public class FlowEx32 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int menu;
+
+        while(true){
+            System.out.println("(1) square");
+            System.out.println("(2) square root");
+            System.out.println("(3) log");
+            System.out.println("원하는 메뉴(1~3)를 선택하세요. (종료:0)>");
+            String input = scanner.nextLine();
+            menu=Integer.parseInt(input);
+
+            if(menu==0){
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }else if(!(1<=menu && menu<=3)){
+                System.out.println("메뉴를 잘못 선택하셨습니다.");
+                continue;
+            }
+            System.out.println("선택하신 메뉴는 "+menu+"번 입니다.");
+        }
+    }
+}
