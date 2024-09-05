1f85f3890a1cacc151b92017ca38cb95d9fabbb2
jiixon <jr0107380@gmail.com>
2024-09-06 02:03:25 +0900

    24.09.06 Upload

--- src/poly/overriding/Child.java
+++ src/poly/overriding/Child.java
@@ -3,6 +3,7 @@ package poly.overriding;
 public class Child extends Parent {
     public String value = "child";
 
+    //메서드 오버라이딩의 중요한점 -> 오버라이드된 메서드가 우선순위를 가진다
     @Override
     public void method(){
         System.out.println("Child.method");
